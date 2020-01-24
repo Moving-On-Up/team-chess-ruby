7243ce8f7974c7f5e7fa1fb14292be5b76af96cf
Chris Howle <cthowle@gmail.com>
2020-01-24 04:17:14 +0000

    Added initial games controller rspec test and updated routes

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,16 +1,13 @@
 class GamesController < ApplicationController
-    def index
-        @game = Game.order("RANDOM()").first
-    end
 
     def new
         @game = Game.new
     end
 
     def create
-        current_user.games.create(game_params)
+        #current_user.games.create(game_params)
         redirect_to root_path
-      end
+    end
     
     def show
         @game = Game.find(params[:id])
--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,11 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
-  describe "games#index action" do
+  describe "games#new action" do
     it "should successfully show the page" do
-      get :index
+      get :new
       expect(response).to have_http_status(:success)
     end
   end
+
+  # describe "games#create action" do
+  #   it "should successfully show the page" do
+  #     get :create
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+
+  # describe "games#show action" do
+  #   it "should successfully show the page" do
+  #     get :show
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
 end
