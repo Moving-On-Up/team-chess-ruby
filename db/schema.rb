0de3916cd768bdce20305d68e7ed0c9ae93beb5f
ljones1877 <jlorin7@gmail.com>
2020-01-31 02:02:14 +0000

    Removed user id from games

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_30_214448) do
+ActiveRecord::Schema.define(version: 2020_01_31_020002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,12 +19,10 @@ ActiveRecord::Schema.define(version: 2020_01_30_214448) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
     t.integer "white_player_id"
     t.integer "black_player_id"
     t.string "current_status"
     t.string "current_user"
-    t.index ["user_id"], name: "index_games_on_user_id"
   end
 
   create_table "pieces", force: :cascade do |t|
