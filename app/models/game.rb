4cd7f70e19d54220933aa8d8b10e1dd517724dfc
ljones1877 <jlorin7@gmail.com>
2020-02-01 19:26:51 +0000

    change the white_player_user_id to white _player_id in game.rb

--- app/models/game.rb
+++ app/models/game.rb
@@ -23,65 +23,65 @@ class Game < ApplicationRecord
     # WHITE PIECES
       # Pawns
       (1..8).each do |x_position|
-        Pawn.create(game_id: id, white: true, x_position: x_position, y_position: 7, user_id: white_player_user_id, name: "Pawn_white")
+        Pawn.create(game_id: id, white: true, x_position: x_position, y_position: 7, user_id: white_player_id, name: "Pawn_white")
       end
 
       # Rooks
       [1, 8].each do |x_position|
-        Rook.create(game_id: id, white: true, x_position: x_position, y_position: 8, user_id: white_player_user_id, name: "Rook_white")
+        Rook.create(game_id: id, white: true, x_position: x_position, y_position: 8, user_id: white_player_id, name: "Rook_white")
       end
 
       # Knights
       [2, 7].each do |x_position|
-        Knight.create(game_id: id, white: true, x_position: x_position, y_position: 8, user_id: white_player_user_id, name: "Knight_white")
+        Knight.create(game_id: id, white: true, x_position: x_position, y_position: 8, user_id: white_player_id, name: "Knight_white")
       end
 
       #Bishops
       [3, 6].each do |x_position|
-        Bishop.create(game_id: id, white: true, x_position: x_position, y_position: 8, user_id: white_player_user_id, name: "Bishop_white")
+        Bishop.create(game_id: id, white: true, x_position: x_position, y_position: 8, user_id: white_player_id, name: "Bishop_white")
       end
 
       #King
-      King.create(game_id: id, white: true, x_position: 5, y_position: 8, user_id: white_player_user_id, name: "King_white")
+      King.create(game_id: id, white: true, x_position: 5, y_position: 8, user_id: white_player_id, name: "King_white")
 
       #Queen
-      Queen.create(game_id: id, white: true, x_position: 4, y_position: 8, user_id: white_player_user_id, name: "Queen_white")
+      Queen.create(game_id: id, white: true, x_position: 4, y_position: 8, user_id: white_player_id, name: "Queen_white")
 
     # BLACK PIECES
       # Pawns
       (1..8).each do |x_position|
-        Pawn.create(game_id: id, white: false, x_position: x_position, y_position: 2, user_id: black_player_user_id, name: "Pawn_black")
+        Pawn.create(game_id: id, white: false, x_position: x_position, y_position: 2, user_id: black_player_id, name: "Pawn_black")
       end
 
       # Rooks
       [1, 8].each do |x_position|
-        Rook.create(game_id: id, white: false, x_position: x_position, y_position: 1, user_id: black_player_user_id, name: "Rook_black")
+        Rook.create(game_id: id, white: false, x_position: x_position, y_position: 1, user_id: black_player_id, name: "Rook_black")
       end
 
       # Knights
       [2, 7].each do |x_position|
-        Knight.create(game_id: id, white: false, x_position: x_position, y_position: 1, user_id: black_player_user_id, name: "Knight_black")
+        Knight.create(game_id: id, white: false, x_position: x_position, y_position: 1, user_id: black_player_id, name: "Knight_black")
       end
 
       #Bishops
       [3, 6].each do |x_position|
-        Bishop.create(game_id: id, white: false, x_position: x_position, y_position: 1, user_id: black_player_user_id, name: "Bishop_black")
+        Bishop.create(game_id: id, white: false, x_position: x_position, y_position: 1, user_id: black_player_id, name: "Bishop_black")
       end
 
       #King
-      King.create(game_id: id, white: false, x_position: 5, y_position: 1, user_id: black_player_user_id, name: "King_black")
+      King.create(game_id: id, white: false, x_position: 5, y_position: 1, user_id: black_player_id, name: "King_black")
 
       #Queen
-      Queen.create(game_id: id, white: false, x_position: 4, y_position: 1, user_id: black_player_user_id, name: "Queen_black")
+      Queen.create(game_id: id, white: false, x_position: 4, y_position: 1, user_id: black_player_id, name: "Queen_black")
   end
 
 
   def white_player
-    User.find_by_id(white_player_user_id)
+    User.find_by_id(white_player_id)
   end
 
   def black_player
-    User.find_by_id(black_player_user_id)
+    User.find_by_id(black_player_id)
   end
 
   def winner
