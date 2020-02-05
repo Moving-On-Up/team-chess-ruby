302ca3b398c7b5c8efc783054e81d7344b62e104
j1wooldridge <jacques.wooldridge@gmail.com>
2020-02-05 16:54:17 +0000

    all updates and changes til 2020-02-05 10:53

--- spec/models/queen_spec.rb
+++ spec/models/queen_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Queen, type: :model do
   let(:current_user) { FactoryBot.create(:user, id: 1) }
-    let(:current_user2) { FactoryBot.create(:user, id: 2) }
-    let(:game) { FactoryBot.create(:game, user_id: current_user.id, turn_player_id: current_user.id, white_player_id: current_user.id, black_player_id: current_user2.id) }
+  let(:current_user2) { FactoryBot.create(:user, id: 2) }
+  let(:game) { FactoryBot.create(:game, user_id: current_user.id, turn_player_id: current_user.id, white_player_id: current_user.id, black_player_id: current_user2.id) }
 
   it "should return true to move diagonally" do
     queen = FactoryBot.create(:queen, x_position:5, y_position: 5, player_id: current_user.id, game_id: game.id, white:false)
