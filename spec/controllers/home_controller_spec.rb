83fa5e012fc8f0fe22c28dda5ef0fae9774f7731
Chris Howle <cthowle@gmail.com>
2020-02-07 03:16:12 +0000

    Fixed one home_controller_spec.rb test

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe HomeController, type: :controller do
   describe "home#index action" do
     it "should successfully show the page" do
+      user = FactoryBot.create(:user)
+      sign_in user
       get :index
       expect(response).to have_http_status(:success)
     end
