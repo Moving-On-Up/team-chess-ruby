49602b8f7766d3503fa3433006463457b1270ec7
ljones1877 <jlorin7@gmail.com>
2020-02-10 18:33:37 +0000

    Added link on config route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,5 @@ Rails.application.routes.draw do
   resources :pieces
 
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'games/:id/:piece_id/:x_position/:y_position', :to => 'games#show', :as => 'show'
-  post 'games/:id/:piece_id/:x_position/:y_position', :to => 'games#move', :as => 'move'
 end
