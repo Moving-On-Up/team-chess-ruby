fca1575ea5d01ae72e43affd51d17f6d8532ec2f
ljones1877 <jlorin7@gmail.com>
2020-02-17 20:09:37 +0000

    Updated rook

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -3,9 +3,32 @@ class Rook < Piece
   def valid_move?(new_x_position, new_y_position, id = nil, color = nil)
     x_distance = x_distance(new_x_position)
     y_distance = y_distance(new_y_position)
-
-    (x_distance >= 1 && y_distance == 0) || 
-    (y_distance >= 1 && x_distance == 0) 
+    (x_distance >= 1 && y_distance == 0) || (y_distance >= 1 && x_distance == 0)
+    
+    tiles = [1, 2, 3, 4, 5, 6, 7]
+    moves = []
+    
+    tiles.each do |x|
+      
+      (x_distance + x > 7)? x_move1 = nil : x_move1 = x_distance + x
+      (x_distance - x < 0)? x_move2 = nil : x_move2 = x_distance - x
+      (y_distance + x > 7)? y_move1 = nil : y_move1 = y_distance + x
+      (y_distance - x < 0)? y_move2 = nil : y_move2 = y_distance - x
+      
+      move1 = [x_move1, y_distance]
+      move2 = [x_move2, y_distance]
+      move3 = [x_distance, y_move1]
+      move4 = [x_distance, y_move2]
+      moves.push(move1, move2, move3, move4)
+    end
+    
+    moves.delete_if {|moves| moves.include?(nil)}
+    moves.delete_if {|moves| !moves.include?([new_x_position, new_y_position])}
+    
+    if moves == []
+      return false
+    else
+      return true
+    end
   end
-
 end
--- spec/models/rook_spec.rb
+++ spec/models/rook_spec.rb
@@ -1,44 +1,36 @@
 require 'rails_helper'
 
-RSpec.describe Knight, type: :model do
+RSpec.describe Rook, type: :model do
   describe "#valid move?" do
     let(:current_user) { FactoryBot.create(:user, id: 1) }
     let(:current_user2) { FactoryBot.create(:user, id: 2) }
     let(:game) { FactoryBot.create(:game, user_id: current_user.id, turn_player_id: current_user.id, white_player_id: current_user.id, black_player_id: current_user2.id) }
   
-    # it 'should return true to move one forward' do
-    #   @rook = FactoryBot.create(:rook, x_position: 5, y_position: 5, player_id: current_user.id, game_id: game.id, white:false)
-    #   expect(@rook.valid_move?(5,6)).to eq true
-    # end
-
-    # it 'should return true to move horizontal' do
-    #   @rook = FactoryBot.create(:rook, x_position: 5, y_position: 5, player_id: current_user.id, game_id: game.id, white:false)
-    #   expect(@rook.valid_move?(6,5)).to eq true
-    # end
- 
-    # it 'should return false to move diagonal' do
-    #   @rook = FactoryBot.create(:rook, x_position: 5, y_position: 5, player_id: current_user.id, game_id: game.id, white:false)
-    #   expect(@rook.valid_move?(6,6)).to eq false
-    # end
 
     it "should return true to move one square forward" do
-    rook = FactoryBot.create(:rook, x_position:5, y_position: 5, player_id: current_user2.id, game_id: game.id, white: false)
-    expect(rook.valid_move?(6, 5)).to eq(true)
-   end
+      rook = FactoryBot.create(:rook, x_position:5, y_position: 5, player_id: current_user2.id, game_id: game.id, white: false)
+      expect(rook.valid_move?(6, 5)).to eq(true)
+    end
 
-   it "should return true to move one square backward" do
-    rook = FactoryBot.create(:rook, x_position:5, y_position: 5, player_id: current_user2.id, game_id: game.id, white: false)
-    expect(rook.valid_move?(4, 5)).to eq(true)
-   end
+    it "should return true to move one square backward" do
+      rook = FactoryBot.create(:rook, x_position:5, y_position: 5, player_id: current_user2.id, game_id: game.id, white: false)
+      expect(rook.valid_move?(4, 5)).to eq(true)
+    end
 
-   it "should return true to move one square up" do
-    rook = FactoryBot.create(:rook, x_position:5, y_position: 5, player_id: current_user2.id, game_id: game.id, white: false)
-    expect(rook.valid_move?(5, 4)).to eq(true)
-   end
+    it "should return true to move one square up" do
+      rook = FactoryBot.create(:rook, x_position:5, y_position: 5, player_id: current_user2.id, game_id: game.id, white: false)
+      expect(rook.valid_move?(5, 4)).to eq(true)
+    end
 
-   it "should return true to move one square down" do
-    rook = FactoryBot.create(:rook, x_position:5, y_position: 5, player_id: current_user2.id, game_id: game.id, white: false)
-    expect(rook.valid_move?(5, 6)).to eq(true)
-   end
+    it "should return true to move one square down" do
+      rook = FactoryBot.create(:rook, x_position:5, y_position: 5, player_id: current_user2.id, game_id: game.id, white: false)
+      expect(rook.valid_move?(5, 6)).to eq(true)
+    end
+
+
+    it "should return false to move to a non-same-color square" do
+      rook = FactoryBot.create(:rook, x_position:5, y_position: 5, player_id: current_user.id, game_id: game.id, white:false)
+      expect(rook.valid_move?(2, 3)).to eq(false)
+    end
   end
 end 
