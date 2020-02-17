f3d09d3d08065d9c26ae9b9e07c474faa3a9bd5a
ljones1877 <jlorin7@gmail.com>
2020-02-17 01:44:25 +0000

    58 test 0 failures

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -4,28 +4,8 @@ class Rook < Piece
     x_distance = x_distance(new_x_position)
     y_distance = y_distance(new_y_position)
 
-    (x_distance >= 1 && y_distance == 0) || (y_distance >= 1 && x_distance == 0)
-    
-    tiles = [1, 2, 3, 4, 5, 6, 7, 8]
-    moves = []
-
-    tiles.each do |x|
-
-      (x_distance + x > 7)? x_move1 = nil : x_move1 = x_distance + x
-      (x_distance - x < 0)? x_move2 = nil : x_move2 = x_distance - x
-      (y_distance + x > 7)? y_move1 = nil : y_move1 = y_distance + x
-      (y_distance - x < 0)? y_move2 = nil : y_move2 = y_distance - x
-
-      move1 = [x_move1, y_distance]
-      move2 = [x_move2, y_distance]
-      move3 = [x_distance, y_move1]
-      move4 = [x_distance, y_move2]
-      moves.push(move1, move2, move3, move4)
-    end
-
-    moves.delete_if {|moves| moves.include?(nil)}
-
-    return moves
+    (x_distance >= 1 && y_distance == 0) || 
+    (y_distance >= 1 && x_distance == 0) 
   end
 
 end
