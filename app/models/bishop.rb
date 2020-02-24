17ea9f2118c61104d1ad69b32a91bfa01ac44ba4
Carriane Linzer <carrianelinzer@icloud.com>
2020-02-24 15:47:03 +0000

    errors

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -5,7 +5,8 @@ class Bishop < Piece
     y_distance = y_distance(new_y_position)
 
     if self.diagonal?(x_distance, y_distance)  
-      move_to(new_x_position, new_y_position)
+      move_to!(new_x_position, new_y_position)
+      self.save
     elsif self.is_obstructed?(x_distance, y_distance)
      return false
     end
