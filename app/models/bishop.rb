37c8a5d7a520866e8d0853e75711d86d063bdf9b
Carriane Linzer <carrianelinzer@icloud.com>
2020-02-24 15:30:10 +0000

    new changes

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -4,17 +4,13 @@ class Bishop < Piece
     x_distance = x_distance(new_x_position)
     y_distance = y_distance(new_y_position)
 
-    (x_distance >= 1 && y_distance >= 1) && diagonal?(x_distance, y_distance)
-  end
-
-  def check_move?()
-    games.pieces.each do |b|
-      if b.diagonal?(x_distance, y_distance) && b.is_obstructed?
-        move_to! #new location
-      else
-        false #alert invalid move!
-      end
+    if self.diagonal?(x_distance, y_distance)  
+      move_to(new_x_position, new_y_position)
+    elsif self.is_obstructed?(x_distance, y_distance)
+     return false
     end
+
+    (x_distance >= 1 && y_distance >= 1) && diagonal?(x_distance, y_distance)
   end
 
 
