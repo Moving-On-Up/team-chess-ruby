01a7a0a8f7b422fa956b0326e7b591870c2a0073
Carriane Linzer <carrianelinzer@icloud.com>
2020-03-11 12:48:23 +0000

    stalemate test?

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,14 +1,11 @@
 class Bishop < Piece
   
-  def valid_move?(new_x_position, new_y_position, id = nil, white = nil)
-    x_dist = x_distance(new_x_position)
-    y_dist = y_distance(new_y_position)
+  def valid_move?(new_x_position, new_y_position, id = nil, color = nil)
+    x_distance = x_distance(new_x_position)
+    y_distance = y_distance(new_y_position)
 
-    if (x_dist >= 1 && y_dist >= 1) && diagonal?(x_dist, y_dist)
-      return true
-    else
-      return false
-    end
+    (x_distance >= 1 && y_distance >= 1) && diagonal?(x_distance, y_distance)
   end
+
 
 end
