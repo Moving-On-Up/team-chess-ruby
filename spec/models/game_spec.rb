402f59372de01df2d229979a57379adf13a3a3c6
Carriane Linzer <carrianelinzer@icloud.com>
2020-03-11 15:24:53 +0000

    first passing test

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,58 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-
+  let(:game) { FactoryBot.create(:game) }
  
   describe "#contains_piece?" do
     it "should return true if the end coordinates contains a piece" do
-    
-    
+    expect(game.contains_piece?(2, 7)).to eq true
     end
   end
 
   describe "#is_in_check" do
     it 'should determine that the game is in check' do
-      
+    
     end
   end
 
   describe "#is_not_in_check" do
     it 'should determine that the game is not in check' do
-     
-    end
-  end
-
-
-
-  describe "#black_turn?" do
-    it 'should determine that player black is up' do
-     
-    end
-  end
-
-  describe "#white_turn?" do
-    it 'should determine that player white is up' do
-    
+  
     end
   end
 
   describe "#stalemate" do
     it 'should detect stalemate' do
-     
-      # White is in stalemate (returns true) and Black is not (returns false)
+    # White is in stalemate (returns true) and Black is not (returns false)
       
     end
   end
 
   describe "#checkmate" do
     it 'should detect checkmate' do
-      
+    
     end
   end
 
   describe "#not_in_checkmate" do
     it 'should detect state is not checkmate' do
-      
+      expect(game.checkmate).to eq false
     end
   end
 end
