806a3d082ec73d0fe193df44c6f2988dc502647c
Carriane Linzer <carrianelinzer@icloud.com>
2020-03-12 07:10:24 +0000

    User dashboard

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -59,7 +59,7 @@ class GamesController < ApplicationController
     private
     
     def game_params
-        params.require(:game).permit(:name)
+        params.require(:game).permit(:name, :email)
     end
 
     def current_game
