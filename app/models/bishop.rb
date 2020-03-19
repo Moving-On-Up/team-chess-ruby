166ec5c49a6129f7e8d5a154cd12770a2e9de82e
Carriane Linzer <carrianelinzer@icloud.com>
2020-03-19 14:43:51 +0000

    changes to bishop

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -3,19 +3,7 @@ class Bishop < Piece
     x_dist = x_distance(new_x_position)
     y_dist = y_distance(new_y_position)
 
-    def is_obstructed?(new_x_position, new_y_position)
-      super
-    end
 
-    if (x_dist >= 1 && y_dist >= 1) && diagonal?(x_dist, y_dist)
-      return true
-    elsif up?(y_dist) || down?(y_dist)
-      return false
-    else 
-      return false
-    end
-  end
-
- 
-  
+    (x_dist >= 1 && y_dist >= 1) && diagonal?(x_dist, y_dist)
+  end 
 end
